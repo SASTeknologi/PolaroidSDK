8619de58574872997449dd404b7e3ca3f2bcb29a
Yaser Arafat <gilbert.arafat@gmail.com>
2022-01-03 17:28:15 +0700

    Initialize Riders Project

---
+++ MainClass.cs
@@ -0,0 +1,7 @@
+﻿namespace PolaroidLibrary;
+
+public class MainClass
+{
+    public bool IsPrinterConnected { get; internal set; } = false;
+    public bool IsReaderConnected { get; internal set; } = false;
+}
